f6182c3a6a6dbefe96e6428f751ecde50c5d528c
Rimmuru <81404937+Rimmuru@users.noreply.github.com>
2023-12-31 07:31:56 +0000

    wip
    wip support for "Lua Plugin (Reloaded)" v20230826.0-Stable
    replaced load script with run (toggling will "unload")
    "safety" improvements

--- 5modsLuaLoader.lua
+++ 5modsLuaLoader.lua
@@ -23,6 +23,18 @@ local function get_key_pressed(keyID)
     end
 end
 
+local pid = player.player_id()
+local infoTable = { -- this is going to suck
+    ["Player"] = {
+        Ped = player.get_player_ped(pid)
+    },
+    ["Vehicle"] = {
+        Id = ped.get_vehicle_ped_is_using(player.get_player_ped(pid))
+    }
+}
+
+local loadedScripts = {}
+
 local function loadScript(scriptName)
     local scriptPath = scriptsPath .. scriptName .. ".lua"
  
@@ -32,14 +44,42 @@ local function loadScript(scriptName)
         return
     end
 
+    -- support for https://www.gta5-mods.com/tools/jm36-lua-plugin-for-script-hook-v-reloaded
+    _G["JM36_GTAV_LuaPlugin_Version"] = 20230724.0
+    _G["JM36"] = _G["JM36"] or {}
+    
+    _G["Info"] = infoTable
+
+    _G["JM36"].yield = function(time)
+        return coroutine.yield(time)
+    end
+    _G["JM36"].Wait = function(time)
+        return coroutine.yield(time)
+    end
+    
+    _G["JM36"].CreateThread = function(callback)
+        return menu.create_thread(callback)
+    end
+
+    _G["JM36"].CreateThread_HighPriority = function(callback)
+        return menu.create_thread(callback)
+    end
+
     _G.get_key_pressed = get_key_pressed
+
+    _G.IsControlJustPressed = function(id, idx)
+        return get_key_pressed(idx)
+    end
+
     _G.wait = function(time)
         return coroutine.yield(time)
     end
+
     _G["io"].open = function(path, mode) 
         local newPath = scriptsPath .. path
         return io.open(newPath, mode)
     end
+
     _G["Libs"] = setmetatable({}, {
         __index = function(t, key)
             return require("lib.5ModsLua.libs." .. key)
@@ -52,21 +92,26 @@ local function loadScript(scriptName)
 
     chunk()
 
-    menu.notify("Loaded "..scriptName, "Lua Loader")
-
-    local scriptEnv = require("lib\\5modsLua\\"..scriptName)
-    if scriptEnv.init ~= nil then
+    local status, scriptEnv = pcall(require, "lib\\5modsLua\\"..scriptName)
+    if not status then
+        menu.notify("Error loading module for script: " .. tostring(scriptEnv), "Error", 7)
+        return
+    end
+    
+    if scriptEnv and type(scriptEnv.init) == "function" then
         scriptEnv.init()
     end
 
-    if scriptEnv.tick ~= nil then
-        local scriptFeat = menu.add_feature(scriptName, "toggle", scriptFeats.id, function(f)
+    if scriptEnv and type(scriptEnv.tick) == "function" then
+        loadedScripts[scriptName] = menu.add_feature(scriptName, "toggle", scriptFeats.id, function(f)
             while f.on do
                 scriptEnv.tick()
                 coroutine.yield(0)
             end
         end)
     end
+
+    menu.notify("Loaded "..scriptName, "Lua Loader")
 end
 
 -- Loading scripts
@@ -76,8 +121,15 @@ do
     for _, scriptFile in ipairs(scripts) do
         local scriptName = scriptFile:gsub("%.lua", "")
         local scriptParent = menu.add_feature(scriptName, "parent", parent.id)
-        menu.add_feature("Load " .. scriptName, "action", scriptParent.id, function(f)
-            loadScript(scriptName)
+        menu.add_feature("Run", "toggle", scriptParent.id, function(f)
+            if f.on then
+                loadScript(scriptName)
+            else
+                if loadedScripts[scriptName] then
+                    --unload logic. package.loaded might be blacklisted though
+                    menu.delete_feature(loadedScripts[scriptName].id, true)
+                end
+            end
         end)
     end
 end
