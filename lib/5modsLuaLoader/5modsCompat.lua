dc9f86198841401d6a2d436baa891fe8e801a6e2
Rimuru <81404937+Rimmuru@users.noreply.github.com>
2024-01-04 14:57:39 +0000

    Update 5modsCompat.lua
    patch 3.5
    support for old native namespace (#5)

--- lib/5modsLuaLoader/5modsCompat.lua
+++ lib/5modsLuaLoader/5modsCompat.lua
@@ -7,27 +7,29 @@ end
 
 function modsCompat()
     _G.get_key_pressed = get_key_pressed
-
+    
     _G.IsControlJustPressed = function(id, idx)
         return get_key_pressed(idx)
     end
-
+    
     _G.wait = function(time)
         return coroutine.yield(time)
     end
-
+    
     _G["io"].open = function(path, mode) 
         local newPath = scriptsPath .. path
         return io.open(newPath, mode)
     end
-
+    
     _G["Libs"] = setmetatable({}, {
         __index = function(t, key)
             return require("lib.5ModsLua.libs." .. key)
         end
     })
     
-    _G["TIME"] = CLOCK --Override time native namespace
+    _G["TIME"] = CLOCK --Override native namespace
+    _G["AI"] = TASK
+
     _G["Keys"] = require("lib\\5modsLuaLoader\\keys")
     _G["keys"] = require("lib\\5modsLuaLoader\\keys")
 end
